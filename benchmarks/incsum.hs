1c688a19a9d58542c64eb3b0d4c59a8caa19b271
Matthew Mole <t-mmole@microsoft.com>
2013-02-15 12:35:25 +0000

    Repa version of incsum seems to be doing much less dense parallel work then DPH

--- benchmarks/incsum.hs
+++ benchmarks/incsum.hs
@@ -18,23 +18,18 @@ incsum :: U.Vector Int -> Int
 incsum xs = U.sum $ U.map (+1) xs
 
 main = do
-       let p10m = PA.fromList [1..10000000]
-       let s10m = U.fromList [1..10000000]
-       let r10m = (RU.fromListUnboxed (Z :. (10000000::Int)) [1..10000000])
+       let p1m = PA.fromList [1..1000000]
+       let s1m = U.fromList [1..1000000]
+       let r1m = (RU.fromListUnboxed (Z :. (1000000::Int)) [1..1000000])
        --let x = r10m `deepSeqArray` s10m `deepseq` 0 -- Has no effect on mean
-       let a = incsum s10m
-       let b = incsumDPH p10m --This seems to benefit completetion times of DPH ~~800ms for 10m to ~~200ms
-       let c = incsumR r10m --This doesn't seem to have an affect on Repa
+       let a = incsum s1m
+       let b = incsumDPH p1m --This seems to benefit completetion times of DPH ~~800ms for 10m to ~~200ms
+       let c = incsumR r1m --This doesn't seem to have an affect on Repa
        putStrLn(show(a +  b + c))
        defaultMainWith cfg (return ()) [
            bgroup "incsum"
-             [ bench "incsumSeq 10mil" $ nf incsum s10m,
-               bench "incsumPar 10mil" $ nf incsumDPH p10m,
-               bench "incsumRep 10mil" $ nf incsumR r10m
-               --,bench "incsumRep 10mil" $ nf incsumR r10m,
-               --bench "incsumPar 10mil" $ nf incsumDPH p10m,
-               --bench "incsumSeq 10mil" $ nf incsum s10m
+             [ bench "incsumSeq 1mil" $ nf incsum s1m,
+               bench "incsumPar 1mil" $ nf incsumDPH p1m,
+               bench "incsumRep 1mil" $ nf incsumR r1m
              ]
          ]
---let result = incsumW (PA.fromList [1..10000000])
---       in print result
