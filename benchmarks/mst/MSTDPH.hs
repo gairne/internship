4d76d02a5707e1f17c30eb0f5fc3b29475ff4a2e
Matthew Mole <t-mmole@microsoft.com>
2013-02-20 14:47:22 +0000

    MST compiling, but segfaulting

--- benchmarks/mst/MSTDPH.hs
+++ benchmarks/mst/MSTDPH.hs
@@ -3,45 +3,36 @@
 
 module MSTDPH (kruskalPA) where
 
-import Data.Array.Parallel
-import qualified Data.Array.Parallel.Prelude.Int as I
+import Data.Array.Parallel as P
+import Data.Array.Parallel.Prelude.Int as I
+import Data.Array.Parallel.Prelude.Bool as B
+import Data.Array.Parallel.Prelude.Tuple as T
 
 type Node = Int
 type Edge = (Node, Node)
 type Graph = [:Edge:]
 
---instance Show Edge where
---  show (E (u, v) w) = "(" ++ show u ++ ", " ++ show v ++ "):" ++ show w
---instance Eq Edge where
---  (==) (E (u, v) w) (y, z) = u == y && v == z && w == x
---  (/=) (E (u, v) w) (y, z) = u /= y || v /= z || w /= x
-
---instance Eq Graph where
---  (==) es fs = comparePArray es fs
---  (/=) es fs = not (comparePArray es fs)
-
---comparePArray :: [:Edge:] -> [:Edge:] -> Bool
---comparePArray [::] [::] = True
---comparePArray (: x : xs :) (: y : ys :) = x == y && comparePArray xs ys
-
 -- Find the sub MST that a node is in or [] if not
 nodeInMSTs :: [:Graph:] -> Node -> Graph
---nodeInMSTs [::] n = [::]
-nodeInMSTs msts n = concatP $ mapP (\mst -> if (nodeInMST n mst) then mst else [::]) msts
---nodeInMSTs (mst:msts) n = if (nodeInMST n mst) then mst else nodeInMSTs n msts
+nodeInMSTs msts n = concatP (mapP (\mst -> if (nodeInMST n mst) then mst else emptyP) msts)
 
 nodeInMST :: Node -> Graph -> Bool
---nodeInMST n [::] = False
-nodeInMST n es = orP (mapP (\e -> n == (fst e) || n == (snd e)) es)
---nodeInMST n (e:es) =
---  | n == (fst e) || n == (snd e) = True
---  | otherwise = nodeInMST n es
+nodeInMST n es = orP (mapP (\e@(n1, n2) -> n I.== n1 B.|| n I.== n2) es)
+
+-- We don't seem to be able to [: a :] == [: a :]
+-- Also we cannot toPArrayP x == toPArrayP y
+-- Therefore here is some horrible code that compares each edge directly 
+notEqualGraph :: Graph -> Graph -> Bool
+notEqualGraph es fs = B.not (andP (mapP (\i -> equalEdge (indexP es i) (indexP fs i)) (enumFromToP 0 (lengthP es I.- 1))))
+
+equalEdge :: Edge -> Edge -> Bool
+equalEdge e@(n1, n2) f@(n3, n4) = n1 I.== n3 B.&& n2 I.== n4
 
 -- Entrypoint. Provide a Graph in the form of PArray Edge and we return [Graph] in the form of PArray Edge
 -- Owing to the limitations (and no toNestedPArrayP), we flatten the [[Edge]] to [Edge]
 --    normally we would return a forest of trees.
 kruskalPA :: PArray Edge -> PArray Edge
-kruskalPA ps = toPArrayP (concatP (kruskal (fromPArrayP ps) [::]))
+kruskalPA ps = toPArrayP (concatP (kruskal (fromPArrayP ps) emptyP))
 {-# NOINLINE kruskalPA #-}
 
 -- Kruskal's Minimum Spanning Tree Algorithm.
@@ -53,25 +44,23 @@ kruskalPA ps = toPArrayP (concatP (kruskal (fromPArrayP ps) [::]))
 --     elif both nodes seen before, ignore this edge
 -- Result: A forest of trees representing the MST.
 kruskal :: [:Edge:] -> [:Graph:] -> [:Graph:]
-kruskal [::] msts = msts
--- Hacky implementation of recursion
--- kruskal (e:es) msts = kruskal es (kruskal' msts e)
-kruskal es msts = kruskal (sliceP 1 (lengthP es) es) (kruskal' msts (es !: 0))
+kruskal es msts
+  | lengthP es I.== 0 = msts
+  | otherwise = kruskal (sliceP 1 (lengthP es I.- 1) es) (kruskal' msts (es !: 0))
 
--- The main logic. Check the status of the two nodes in an edge and act on the MSTs accordingly.
+---- The main logic. Check the status of the two nodes in an edge and act on the MSTs accordingly.
 kruskal' :: [:Graph:] -> Edge -> [:Graph:]
-kruskal' msts e
-  -- Both Nodes do not exist in any of the MST subtrees. Therefore create a new subtree
-  | lengthP leftNodeMST == 0 && lengthP rightNodeMST == 0 = (singletonP (singletonP e)) +:+ msts
-  -- One node exists in a subtree but not the other. Add the other node to the subtree
-  | lengthP leftNodeMST == 0  = singletonP ((singletonP e) +:+ rightNodeMST) +:+ (filterP (\mst -> (toPArrayP mst) /= (toPArrayP rightNodeMST)) msts)
-  | lengthP rightNodeMST == 0 = singletonP ((singletonP e) +:+ leftNodeMST) +:+ (filterP (\mst -> (toPArrayP mst) /= (toPArrayP leftNodeMST)) msts)
-  -- Both nodes exist in different subtrees
-  | (toPArrayP leftNodeMST) /= (toPArrayP rightNodeMST) = singletonP ((singletonP e) +:+ (rightNodeMST+:+leftNodeMST)) +:+ (filterP (\mst -> (toPArrayP mst) /= (toPArrayP leftNodeMST) && (toPArrayP mst) /= (toPArrayP rightNodeMST)) msts)
-  -- Both nodes already exist in the same subtree
+kruskal' msts e@(n1,n2)
+--  -- Both Nodes do not exist in any of the MST subtrees. Therefore create a new subtree
+  | lengthP leftNodeMST I.== 0 && lengthP rightNodeMST I.== 0 = (singletonP (singletonP e)) +:+ msts
+--  -- One node exists in a subtree but not the other. Add the other node to the subtree
+  | lengthP leftNodeMST I.== 0  = singletonP ((singletonP e) +:+ rightNodeMST) +:+ (filterP (\mst -> notEqualGraph mst rightNodeMST) msts)
+  | lengthP rightNodeMST I.== 0 = singletonP ((singletonP e) +:+ leftNodeMST) +:+ (filterP (\mst -> notEqualGraph mst leftNodeMST) msts)
+--  -- Both nodes exist in different subtrees
+  | (notEqualGraph leftNodeMST rightNodeMST) = singletonP ((singletonP e) +:+ (rightNodeMST+:+leftNodeMST)) +:+ (filterP (\mst -> (notEqualGraph mst leftNodeMST) B.&& (notEqualGraph mst rightNodeMST)) msts)
+--  -- Both nodes already exist in the same subtree
   | otherwise = msts
   where
-    [:leftNodeMST, rightNodeMST:] = mapP (nodeInMSTs msts) [: fst e, snd e :]
-    --leftNodeMST = nodeInMSTs (fst e) msts
-    --rightNodeMST = nodeInMSTs (snd e) msts
+    leftNodeMST = nodeInMSTs msts n1
+    rightNodeMST = nodeInMSTs msts n2
 
