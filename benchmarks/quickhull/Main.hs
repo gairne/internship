8c2699c7d5c5e7df80e630712f44a24e94e8acaa
Matthew Mole <t-mmole@microsoft.com>
2013-03-26 11:22:09 +0000

    Quickhull Main.hs is now consistent with Quicksort

--- benchmarks/quickhull/Main.hs
+++ benchmarks/quickhull/Main.hs
@@ -1,18 +1,19 @@
 import Timing
 import System.Environment
 import Randomish
-
 import Point
-import DPH
-import Repa
 
 import Data.Vector.Unboxed as VU
+
 import Data.Array.Parallel
 import Data.Array.Parallel.PArray as PA hiding (nf)
-import qualified Data.Array.Parallel.PArray.Scalar as PS
+import Data.Array.Parallel.PArray.Scalar as PS
 
 import Data.Array.Repa as R
 import Data.Array.Repa.Repr.Unboxed as RU
+
+import Vectorised
+import Repa
 
 xMin :: Double
 xMin = 0.0
@@ -44,17 +45,17 @@ run alg nPoints
            vec2 :: VU.Vector Double
            vec2 = randomishDoubles nPoints yMin yMax seed2
        
-       (result, tme) <- runAlg alg vec1 vec2 nPoints
+       (result, tme) <- runAlg alg vec1 vec2
        putStr $ prettyTime tme
 
-runAlg "dph" vec1 vec2 n
+runAlg "dph" vec1 vec2
   = do let dph1 = PS.fromUArray2 (VU.zip vec1 vec2)
        dph1 `seq` return ()
        time $ let result = quickHullPA dph1 in result `seq` return ()
 
-runAlg "repa" vec1 vec2 n
-  = do let rep1 = RU.fromUnboxed (R.ix1 n) (VU.zip vec1 vec2)
+runAlg "repa" vec1 vec2
+  = do let rep1 = RU.fromUnboxed (R.ix1 (VU.length vec1)) (VU.zip vec1 vec2)
        rep1 `seq` return ()
-       time $ let result = quickHullR rep1 in ((size (extent result))) `seq` return ()
+       time $ let result = quickHullR rep1 in (size (extent result)) `seq` return ()
 
 
